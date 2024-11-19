e921e22acfd97e5ca7a0e3c60b3d3a14f53796cc
jvictordev1 <josevictorcruzrb@gmail.com>
2024-11-19 10:13:43 -0300

    feat: home page + header component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { MenuComponent } from './views/menu/menu.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'menu',
+    component: MenuComponent,
+  },
+  { path: '', component: HomeComponent },
+];
