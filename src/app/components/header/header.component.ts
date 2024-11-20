279e81a3f3b56539c31ac6556056c93c2ac350de
jvictordev1 <josevictorcruzrb@gmail.com>
2024-11-20 11:46:41 -0300

    feat: responsiveness

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css', '/src/styles.css'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  hidden = true;
+  handleMenuClick() {
+    this.hidden = !this.hidden;
+  }
+}
